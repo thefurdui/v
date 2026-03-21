2561908513e2be80e3c08161811f4d60b6e0cfb6
Andrei <thefurdui@gmail.com>
2026-03-21 12:42:07 +0500

    feat: add russian language langmap to vim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.cmdheight = 0
+
+-- Map Russian keyboard layout to English QWERTY in normal/visual modes
+vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,"
+  .. "фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+
+-- Ensure that mapped characters trigger your custom keybinds
+vim.opt.langremap = true
