d8b613465b049e05fe87468dc18aff9f8cd3c921
Andrei <thefurdui@gmail.com>
2026-04-18 16:45:14 +0500

    feat(formatting): add sql_formatter

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sql_formatter" },
+      },
+      formatters = {
+        sql_formatter = {
+          prepend_args = { "-l", "sqlite" },
+        },
+      },
+    },
+  },
+}
